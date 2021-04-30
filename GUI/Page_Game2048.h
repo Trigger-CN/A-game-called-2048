c6cd6ba1260ba9360b45239ea57b4407578be333
Trigger-CN <58842678+Trigger-CN@users.noreply.github.com>
2021-04-30 17:44:57 +0800

    upload files

---
+++ GUI/Page_Game2048.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "lvgl/lvgl.h"
+#include "lv_examples/lv_examples.h"
+#include "lv_drivers/win32drv/win32drv.h"
+#include "lv_ext/lv_obj_ext_func.h"
+#include "Matrix/matrix.h"
+#include "stdlib.h"
+
+extern "C" {
+    LV_FONT_DECLARE(Font_MicrosoftYaHei_16);
+    LV_FONT_DECLARE(Font_MicrosoftYaHei_20);
+    LV_FONT_DECLARE(Font_MicrosoftYaHei_28);
+    LV_FONT_DECLARE(Font_MicrosoftYaHei_50);
+}
+
+void GameSetup();
